f295e1939bad771a39aff4462817679f4df90c00
Aksel <aksel.christoffer.wiig@gmail.com>
2025-08-21 22:01:35 +0200

    phone

--- src/components/ClickEffect.tsx
+++ src/components/ClickEffect.tsx
@@ -1,44 +1,76 @@
 import { useState, type JSX } from "react";
 import hjerte from "../assets/hjerte_1.svg";
+import hjerte2 from "../assets/hjerte_2.svg"
 
 const ImageAtClick = ({ children, ...rest }: { children: JSX.Element }) => {
-  const [fadeIn, setFadeIn] = useState(false);
-  const [clickPosition, setClickPosition] = useState<{
+  const [clickPositions, setClickPositions] = useState<{
+    id: number;
+    heartIndex: number;
     x: number;
     y: number;
-  } | null>(null);
+    fadeIn: boolean;
+  }[]>([]);
+  const hearts = [hjerte, hjerte2]
 
-  console.log("clickPosition", clickPosition);
 
   const handleClick = (e: React.MouseEvent) => {
-    setClickPosition({ x: e.clientX, y: e.clientY });
-    setFadeIn(true);
+    const id = Date.now() + Math.random();
+    setClickPositions((previousPositions) => [...previousPositions, { id, heartIndex: getRandomHeart(), x: e.clientX, y: e.clientY, fadeIn: false }]);
 
-    setTimeout(() => setFadeIn(false), 250);
+    requestAnimationFrame(() => {
+      setClickPositions(prev =>
+        prev.map(p => (p.id === id ? { ...p, fadeIn: true } : p))
+      );
+    });
   };
 
-  return (
-    <div onClick={handleClick} {...rest}>
-      {clickPosition && (
-        <img
-          src={hjerte.src}
-          width={hjerte.width}
-          height={hjerte.height}
-          alt="Astro logo"
-          style={{
-            position: "absolute",
-            top: clickPosition.y,
-            left: clickPosition.x,
-            transform: "translate(-50%, -100%)",
-            transition: "opacity 300ms linear",
-            opacity: fadeIn ? 1 : 0,
-            pointerEvents: "none",
-          }}
-        />
-      )}
+  const getRandomHeart = () => {
+    return Math.floor(Math.random() * 2)
+  }
 
-      {children}
-    </div>
+  return (
+    <>
+      <style>
+        {`
+        @keyframes fadeinout {
+          0% { opacity: 0; }
+          14% { opacity: 1; }
+          86% { opacity: 1; }
+          100% { opacity: 0; }
+        }
+
+        .fade-in-out {
+          animation: fadeinout 5s linear forwards;
+          opacity: 0;
+          position: absolute;
+          width: 100px;
+          height: 100px;
+          pointer-events: none;
+        }
+      `}
+      </style>
+      <div onClick={handleClick} {...rest}>
+        {clickPositions?.map((clickPosition) => {
+          return (
+            <img
+              src={hearts[clickPosition.heartIndex].src}
+              width={hearts[clickPosition.heartIndex].width}
+              height={hearts[clickPosition.heartIndex].height}
+              className='fade-in-out'
+              alt="Astro logo"
+              style={{
+                position: "absolute",
+                top: clickPosition.y,
+                left: clickPosition.x,
+                transform: "translate(-50%, -60%)",
+              }}
+            />)
+        })
+        }
+
+        {children}
+      </div>
+    </>
   );
 };
 
