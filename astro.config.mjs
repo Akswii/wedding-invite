a1c3a55efd6aa9d7427cf915d45e371bfef25aeb
Aksel <aksel.christoffer.wiig@gmail.com>
2025-08-26 23:13:54 +0200

    update url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "annaogaksel.no",
+  site: "https://annaogaksel.no",
   base: "/wedding-invite",
   integrations: [react()],
 });
